a9e46ae99d8dc6ad3209718d4e5682af05cffab3
EliazarAgwaya <eliazar.magero@student.moringaschool.com>
2022-09-02 11:48:40 +0300

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with? ("?")
   end
 
   def exclamation?
-
+    self.end_with? ("!")
   end
 
-  def count_sentences
-
+  def count_sentences str
+    str.split(/.!?]+(?=\s|\z)/).count
+    # (str.split(/(\?|\.|!)/)).count()
   end
 end
+word= String.new
+word.sentence?
+puts word.count_sentences("My name is Eliazar. I com from Oyugis!")
